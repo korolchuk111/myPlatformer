b02ea6a3c72a17b0a1106f69aa7b2307f947db69
korolchuk111 <anna.korolchuk@oa.edu.ua>
2021-12-18 14:58:19 +0200

    меню паузи

--- TestProject/Assets/Scprits/PauseButton.cs
+++ TestProject/Assets/Scprits/PauseButton.cs
@@ -1,24 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseButton : MonoBehaviour
 {
-    private bool isPause = false;
-
-    [SerializeField]
-    private GameObject pausePanel;
+    public bool isPause;
+    public GameObject pausePanel;
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+            //відкриється по настисканню на esc також
+        {
+            if (isPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+            
+        }
+        
+    }
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        isPause = false;
+    }
     public void Pause()
     {
-        if (isPause)
-        {
-            pausePanel.SetActive(false);
-            Time.timeScale = 1;
-            isPause = false;
-        }
-        else
-        {
-            pausePanel.SetActive(true);
-            Time.timeScale = 0;
-            isPause = true;
-        }
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        isPause = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("GameMenu");
     }
 }
